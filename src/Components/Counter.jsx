afc5e66acd1f4194fb0783ab975edb03e91960c6
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-02-26 15:11:51 +0600

    added count

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -2,18 +2,37 @@ import { useDispatch, useSelector } from "react-redux";
 import {
   increment,
   decrement,
+  incrementByValue,
 } from "../Redux/Features/Counters/CounterSlice.js";
 
 const Counter = () => {
   const { count } = useSelector((state) => state.counter);
   const dispatch = useDispatch();
-  // console.log(count);
+  console.log(count);
 
   return (
     <>
-      <button onClick={() => dispatch(increment())}>Increment</button>
-      <h1>{count}</h1>
-      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <div className="flex justify-evenly items-center">
+        <button
+          className="px-5 py-3 bg-sky-600 text-white text-2xl font-bold rounded-lg"
+          onClick={() => dispatch(incrementByValue(5))}
+        >
+          Increment by 5
+        </button>
+        <button
+          className="px-5 py-3 bg-sky-600 text-white text-2xl font-bold rounded-lg"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <h1 className="text-black font-bold text-2xl">{count}</h1>
+        <button
+          className="px-5 py-3 bg-sky-600 text-white text-2xl font-bold rounded-lg"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
     </>
   );
 };
