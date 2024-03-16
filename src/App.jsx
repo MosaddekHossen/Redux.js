a76596f770eb3e14125a685ac40046b72c784581
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-03-17 03:00:21 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import Home from "./Pages/Home"
+import Home from "./Pages/Home";
 
 function App() {
-
   return (
     <>
-      <h1 className="text-3xl font-bold text-green-500 text-center underline">Redux</h1>
-      <Home />
+      <div className="max-w-7xl mx-auto space-y-5">
+        <h1 className="text-3xl font-bold text-green-500 text-center underline">
+          Redux
+        </h1>
+        <Home />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -12,7 +12,7 @@ const Counter = () => {
 
   return (
     <>
-      <div className="flex justify-evenly items-center">
+      <div className="grid mx-auto grid-cols-1 space-y-3 md:space-y-0 md:grid-cols-3 lg:grid-cols-4 gap-5 items-center">
         <button
           className="px-5 py-3 bg-sky-600 text-white text-2xl font-bold rounded-lg"
           onClick={() => dispatch(incrementByValue(5))}
@@ -25,7 +25,9 @@ const Counter = () => {
         >
           Increment
         </button>
-        <h1 className="text-black font-bold text-2xl">{count}</h1>
+        <div className="flex justify-center">
+          <h1 className="text-black font-bold text-2xl">{count}</h1>
+        </div>
         <button
           className="px-5 py-3 bg-sky-600 text-white text-2xl font-bold rounded-lg"
           onClick={() => dispatch(decrement())}
