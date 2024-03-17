32d1de061f5a044a9ee464011bd6da41a1a0f6bb
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-03-18 01:54:12 +0600

    last commit

--- src/Redux/Store.jsx
+++ src/Redux/Store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./Features/Counters/CounterSlice";
+import logger from "./Meddlewares/Logger";
 
 export const Store = configureStore({
-    reducer: {
-        counter: counterReducer,
-    }
-})
+  reducer: {
+    counter: counterReducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
